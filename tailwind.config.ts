717a90e43aa0a1e4251297f6e3c83d3c1f97cd87
Npeka <npkhang22@clc.fitus.edu.vn>
2024-11-01 22:42:23 +0700

    build: config and add new framework

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                background: 'var(--background)',
+                foreground: 'var(--foreground)',
+            },
+            fontFamily: {
+                mouse: ['Mouse Memoirs', 'sans-serif'],
+                poppins: ['Poppins', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
