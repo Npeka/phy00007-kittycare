1597846b31226d250c59870d18d7728fbef61466
Npeka <npkhang22@clc.fitus.edu.vn>
2024-12-13 23:29:20 +0700

    feat: add input ip to stream camera

--- src/ui/pet-monitoring/camera-live.tsx
+++ src/ui/pet-monitoring/camera-live.tsx
@@ -1,9 +1,38 @@
-import { Box, Typography, Card, CardMedia } from '@mui/material';
+'use client';
+
+import { useEffect, useState } from 'react';
+import {
+    Box,
+    Typography,
+    Card,
+    CardMedia,
+    TextField,
+    Button,
+} from '@mui/material';
 import { TitleSection } from '@/ui/common';
 
 export default function CameraLive() {
-    const streamUrl =
-        'https://www.youtube.com/live/6TtLd9O1Ajg?si=kDoSB58vTJWvexjJ';
+    const [streamUrl, setStreamUrl] = useState('');
+    const [ipAddress, setIpAddress] = useState('');
+    const [loading, setLoading] = useState(true);
+
+    const handleIpChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setIpAddress(event.target.value);
+    };
+
+    const handleSubmit = () => {
+        if (ipAddress) {
+            setStreamUrl(`http://${ipAddress}`);
+            setLoading(true);
+        }
+    };
+
+    useEffect(() => {
+        if (streamUrl) {
+            setLoading(false);
+        }
+    }, [streamUrl]);
+
     return (
         <>
             <TitleSection>
@@ -14,28 +43,53 @@ export default function CameraLive() {
                 })}
             </TitleSection>
 
+            <Box sx={{ mb: 2 }}>
+                <TextField
+                    label="Nhập IP Camera"
+                    variant="outlined"
+                    fullWidth
+                    value={ipAddress}
+                    onChange={handleIpChange}
+                    sx={{ mb: 2 }}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={handleSubmit}
+                    fullWidth
+                >
+                    Xác nhận
+                </Button>
+            </Box>
+
             <Box sx={{ position: 'relative' }}>
-                <Card sx={{ position: 'relative', borderRadius: 2 }}>
-                    <CardMedia
-                        className="aspect-video w-full rounded-2xl"
-                        component="iframe"
-                        allowFullScreen
-                        src={streamUrl}
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    />
-                    <Box
-                        sx={{
-                            position: 'absolute',
-                            top: 10,
-                            left: 10,
-                            backgroundColor: '#fff',
-                            padding: '4px 8px',
-                            borderRadius: 1,
-                        }}
-                    >
-                        <Typography variant="caption">Trực tiếp</Typography>
-                    </Box>
-                </Card>
+                {loading ? (
+                    <Typography variant="h6" sx={{ textAlign: 'center' }}>
+                        Đang tải...
+                    </Typography>
+                ) : (
+                    <Card sx={{ position: 'relative', borderRadius: 2 }}>
+                        <CardMedia
+                            className="aspect-video w-full rounded-2xl"
+                            component="iframe"
+                            allowFullScreen
+                            src={streamUrl}
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        />
+                        <Box
+                            sx={{
+                                position: 'absolute',
+                                top: 10,
+                                left: 10,
+                                backgroundColor: '#fff',
+                                padding: '4px 8px',
+                                borderRadius: 1,
+                            }}
+                        >
+                            <Typography variant="caption">Trực tiếp</Typography>
+                        </Box>
+                    </Card>
+                )}
             </Box>
         </>
     );
