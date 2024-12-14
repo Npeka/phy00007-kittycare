78726a817744e3048014878923320261a3cc90c8
Npeka <npkhang22@clc.fitus.edu.vn>
2024-12-14 13:32:18 +0700

    feat: integrate ultrasonic sensor for protection feature

---
+++ arduino/Protect.cpp
@@ -0,0 +1,59 @@
+#include "Protect.h"
+
+Protect::Protect(
+    const uint8_t &trigPin,
+    const uint8_t &echoPin,
+    const uint8_t &ledPin,
+    const uint8_t &buzzerPin) : trigPin(trigPin),
+                                echoPin(echoPin),
+                                ledPin(ledPin),
+                                buzzerPin(buzzerPin),
+                                distance(0),
+                                status(false) {}
+
+void Protect::init() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
+
+  digitalWrite(ledPin, LOW);
+  digitalWrite(buzzerPin, LOW);
+}
+
+void Protect::readDistanceCM() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  distance = pulseIn(echoPin, HIGH) * 0.034 / 2;
+}
+
+bool Protect::isFar() const {
+  return distance > 10;
+}
+
+void Protect::on() {
+  digitalWrite(ledPin, HIGH);
+  // tone(buzzerPin, 50);
+  status = true;
+}
+
+void Protect::off() {
+  digitalWrite(ledPin, LOW);
+  // tone(buzzerPin, 0);
+  status = false;
+}
+
+int Protect::getDistance() const {
+  return distance;
+}
+
+bool Protect::getStatus() const {
+  return status;
+}
+
+String Protect::getName() const {
+  return "protect";
+}
---
+++ arduino/Protect.h
@@ -0,0 +1,31 @@
+#ifndef PROTECT_H
+#define PROTECT_H
+
+#include <Arduino.h>
+
+class Protect {
+private:
+  uint8_t trigPin;
+  uint8_t echoPin;
+  uint8_t ledPin;
+  uint8_t buzzerPin;
+  uint8_t distance;
+  bool status;
+
+public:
+  Protect(const uint8_t &trigPin,
+          const uint8_t &echoPin,
+          const uint8_t &ledPin,
+          const uint8_t &buzzerPin);
+  ~Protect() = default;
+  void init();
+  void readDistanceCM();
+  bool isFar() const;
+  void on();
+  void off();
+  int getDistance() const;
+  bool getStatus() const;
+  String getName() const;
+};
+
+#endif
